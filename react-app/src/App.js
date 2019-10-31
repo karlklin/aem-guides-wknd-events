a1b8956bbd9012e0b5f7d8529f0fa8c5aedaa15e
Karol Pawelski <karol.pawelski@cognifide.com>
2019-10-31 14:45:01 +0100

    mix content

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Page, withModel} from '@adobe/cq-react-editable-components';
-import Header from './components/header/Header';
+import { Page, withModel } from '@adobe/cq-react-editable-components';
 
 // This component is the application entry point
 class App extends Page {
@@ -8,7 +7,6 @@ class App extends Page {
     render() {
         return (
             <div className="App">
-                <Header navigationRoot="/content/wknd-events/react/home" />
                     { this.childComponents }
                     { this.childPages }
             </div>
